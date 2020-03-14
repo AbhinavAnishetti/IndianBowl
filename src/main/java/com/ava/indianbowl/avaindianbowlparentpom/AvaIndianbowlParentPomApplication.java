a4e0fc0d9c0372f60b507285430bb307327188d6
AbhinavAnishetti <abhinau.c@gmail.com>
2020-03-14 18:14:31 -0500

    adding Project setup.

---
+++ src/main/java/com/ava/indianbowl/avaindianbowlparentpom/AvaIndianbowlParentPomApplication.java
@@ -0,0 +1,13 @@
+package com.ava.indianbowl.avaindianbowlparentpom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaIndianbowlParentPomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaIndianbowlParentPomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ava/indianbowl/avaindianbowlparentpom/AvaIndianbowlParentPomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ava.indianbowl.avaindianbowlparentpom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaIndianbowlParentPomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

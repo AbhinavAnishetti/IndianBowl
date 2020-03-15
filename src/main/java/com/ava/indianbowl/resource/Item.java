4dc11ad3401e08737134d5ed80a9169cb8e66ab6
AbhinavAnishetti <abhinau.c@gmail.com>
2020-03-14 21:10:26 -0500

    Added Item Resources.

--- src/main/java/com/ava/indianbowl/resource/Item.java
+++ src/main/java/com/ava/indianbowl/resource/Item.java
@@ -8,6 +8,92 @@ public class Item {
     private int  itemCount;
     private long cartId;
     private double totalPrice;
-    
 
+    public Item(Builder builder){
+        this.cartId = builder.cartId;
+        this.fillingCount = builder.fillingCount;
+        this.fillingId = builder.fillingId;
+        this.itemCount = builder.itemCount;
+        this.itemId = builder.itemId;
+        this.productId = builder.productId;
+        this.totalPrice = builder.totalPrice;
+    }
+
+    public long getItemId() {
+        return itemId;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public long getFillingId() {
+        return fillingId;
+    }
+
+    public int getFillingCount() {
+        return fillingCount;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public long getCartId() {
+        return cartId;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemId=" + itemId +
+                ", productId=" + productId +
+                ", fillingId=" + fillingId +
+                ", fillingCount=" + fillingCount +
+                ", itemCount=" + itemCount +
+                ", cartId=" + cartId +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+
+    public static class Builder{
+       private long itemId;
+       private long productId;
+       private long fillingId;
+       private int  fillingCount;
+       private int  itemCount;
+       private long cartId;
+       private double totalPrice;
+
+       public Builder(long itemId, long fillingId, long productId, double totalPrice){
+           itemId = itemId;
+           productId = productId;
+           fillingId = fillingId;
+           totalPrice = totalPrice;
+       }
+
+       public Builder setFillingCount(int fillingCount){
+           this.fillingCount = fillingCount;
+           return this;
+       }
+
+       public Builder setItemCount(int itemCount){
+           this.itemCount = itemCount;
+           return this;
+       }
+
+       public Builder setCartId(long cartId){
+           this.cartId = cartId;
+           return this;
+       }
+
+       public Item Build(){
+           return new Item(this);
+       }
+
+    }
 }
---
+++ src/main/java/com/ava/indianbowl/resourcecontroller/ItemController.java
@@ -0,0 +1,33 @@
+package com.ava.indianbowl.resourcecontroller;
+
+import com.ava.indianbowl.resource.Item;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemController {
+
+    List<Item> itemList = new ArrayList<Item>();
+
+    public List<Item> getItems(){
+        return itemList;
+    }
+
+    public List<Item> getItemsById(){
+        return itemList;
+    }
+
+    public long createItem(Item item){
+        itemList.add(item);
+        return item.getItemId();
+    }
+
+    public String updateItemById(){
+        return "update Item was called";
+    }
+
+    public String deleteItemById(){
+        return "delete Item was called";
+    }
+}
